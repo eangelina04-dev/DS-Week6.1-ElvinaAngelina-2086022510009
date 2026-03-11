67b21938c58cf14b04fd481a225e3d70a1b9fe08
Elvina Angelina <eangelina04@student.ciputra.ac.id>
2026-03-11 15:36:35 +0800

    commit

---
+++ TotallyNotRadicalClub.java
@@ -0,0 +1,39 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class TotallyNotRadicalClub {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Queue<String> queue = new PriorityQueue<>();
+
+        System.out.println("Input list of the age:");
+        String inputLine = scanner.nextLine();
+
+        String[] ageStrings = inputLine.split(" ");
+        for (String token : ageStrings) {
+            queue.add(token);
+        }
+        
+        Queue<String> valid = new PriorityQueue<>();
+        Queue<String> invalid = new PriorityQueue<>();
+
+        while (!queue.isEmpty()) {
+            String ageStr = queue.poll();
+            int age = Integer.parseInt(ageStr);
+            if (age >= 28 && age <= 118) {
+                valid.add(ageStr);
+                }
+            else {
+                invalid.add(ageStr);
+
+                }
+             }
+        System.out.println("Valid ages: " + valid);
+        System.out.println("Invalid ages: " + invalid);
+
+        scanner.close();
+        }
+    }
+
+
